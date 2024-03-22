d0137023400da2d887570d54b98b79836030c434
David <kontakt@david-grass-feria.de>
2024-03-22 22:01:41 +0100

    Update ServicePolicy.php

--- src/Policies/ServicePolicy.php
+++ src/Policies/ServicePolicy.php
@@ -3,14 +3,15 @@
 namespace GrassFeria\StarterkidService\Policies;
 
 use App\Models\User;
-use \GrassFeria\StarterkidService\Models\Service;
 use Illuminate\Auth\Access\Response;
+use \GrassFeria\StarterkidService\Models\Service;
+use GrassFeria\Starterkid\Traits\EditorPolicyTrait;
 use GrassFeria\Starterkid\Traits\OnlyUserRecordPolicyTrait;;
 
 class ServicePolicy
 {
    
-     use OnlyUserRecordPolicyTrait;
+     use EditorPolicyTrait;
 
    
 
