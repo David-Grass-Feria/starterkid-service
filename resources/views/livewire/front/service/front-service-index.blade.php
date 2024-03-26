16f0c12d5029a64c6ac0223051dd4419ceadde29
David <kontakt@david-grass-feria.de>
2024-03-26 17:32:09 +0100

    Update front-service-index.blade.php

--- resources/views/livewire/front/service/front-service-index.blade.php
+++ resources/views/livewire/front/service/front-service-index.blade.php
@@ -13,11 +13,11 @@
         
         <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 xl:grid-cols-3 mt-5">
             @foreach($services as $service)
-            <div class="relative flex items-center space-x-3 rounded-lg border border-gray-300 bg-white px-6 py-5 hover:bg-primary">
+            <div class="relative flex items-center space-x-3 rounded-lg border border-gray-300 bg-white px-6 py-5">
               <div class="flex-shrink-0">
                 <img class="h-10 w-10 rounded-3xl object-contain" src="{{Cache::has('logo') ? Cache::get('logo') : asset('/logo.png')}}" alt="{{$service->name}}">
               </div>
-              <div class="min-w-0 flex-1 text-font_primary hover:text-white">
+              <div class="min-w-0 flex-1 text-font_primary">
                 <a href="{{route('front.service.show',$service->slug)}}" title="{{$service->name}}" class="focus:outline-none">
                   <span class="absolute inset-0" aria-hidden="true"></span>
                   <p class="text-lg font-bold">{{$service->name}}</p>
