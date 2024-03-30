2b08f2caeaf64f5890e1f461df541c47fe74afad
David <kontakt@david-grass-feria.de>
2024-03-31 00:41:29 +0100

    Update Service.php

--- src/Models/Service.php
+++ src/Models/Service.php
@@ -108,11 +108,35 @@ class Service extends Model implements HasMedia
     public function registerMediaConversions(?Media $media = null): void
     {
         $this->addMediaConversion('thumb')
-              ->width(200);
+              ->width(config('starterkid.image_conversions.thumb.width'))
+              ->sharpen(config('starterkid.image_conversions.thumb.sharpen'))
+              ->quality(config('starterkid.image_conversions.thumb.quality'))
+              ->format('webp');
         $this->addMediaConversion('medium')
-              ->width(800);
-        $this->addMediaConversion('large')
-              ->width(1200);
+              ->width(config('starterkid.image_conversions.medium.width'))
+              ->sharpen(config('starterkid.image_conversions.medium.sharpen'))
+              ->quality(config('starterkid.image_conversions.medium.quality'))
+              ->format('webp');
+       $this->addMediaConversion('large')
+              ->width(config('starterkid.image_conversions.large.width'))
+              ->sharpen(config('starterkid.image_conversions.large.sharpen'))
+              ->quality(config('starterkid.image_conversions.large.quality'))
+              ->format('webp');
+              
+        $this->addMediaConversion('thumb_fallback')
+              ->width(config('starterkid.image_conversions.thumb.width'))
+              ->sharpen(config('starterkid.image_conversions.thumb.sharpen'))
+              ->quality(config('starterkid.image_conversions.thumb.quality'));
+              
+        $this->addMediaConversion('medium_fallback')
+              ->width(config('starterkid.image_conversions.medium.width'))
+              ->sharpen(config('starterkid.image_conversions.medium.sharpen'))
+              ->quality(config('starterkid.image_conversions.medium.quality'));
+          
+       $this->addMediaConversion('large_fallback')
+              ->width(config('starterkid.image_conversions.large.width'))
+              ->sharpen(config('starterkid.image_conversions.large.sharpen'))
+              ->quality(config('starterkid.image_conversions.large.quality'));
               
     }
 }
