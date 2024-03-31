8628647c940c08c68116b1e4dd554b23aa71536e
David <kontakt@david-grass-feria.de>
2024-03-31 14:47:17 +0200

    Update front-service-index.blade.php

--- resources/views/livewire/front/service/front-service-index.blade.php
+++ resources/views/livewire/front/service/front-service-index.blade.php
@@ -11,23 +11,20 @@
     <x-starterkid-frontend::wrapper>
    
         
-        <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 xl:grid-cols-3 mt-5">
+    <x-starterkid-frontend::card-grid>
             @foreach($services as $service)
-            <div class="relative flex items-center space-x-3 rounded-lg border border-gray-300 bg-white px-6 py-5">
-              <div class="flex-shrink-0">
-                <img class="h-10 w-10 rounded-3xl object-contain" src="{{Cache::has('logo') ? Cache::get('logo') : asset('/logo.png')}}" alt="{{$service->name}}">
-              </div>
-              <div class="min-w-0 flex-1 text-font_primary">
-                <a href="{{route('front.service.show',$service->slug)}}" title="{{$service->name}}" class="focus:outline-none">
-                  <span class="absolute inset-0" aria-hidden="true"></span>
-                  <p class="text-lg font-bold">{{$service->name}}</p>
-                  <p class="truncate text-xs">{!!Str::limit($service->preview,200)!!}</p>
-                </a>
-              </div>
-            </div>
+            <x-starterkid-frontend::card-grid-service-item 
+            imgSrc="{{Cache::has('logo') ? Cache::get('logo') : asset('/logo.png')}}"
+            imgAlt="{{$service->name}}"
+            href="{{route('front.service.show',$service->slug)}}"
+            hrefTitle="{{$service->name}}"
+            heading="{{$service->name}}"
+            preview="{!!Str::limit($service->preview,200)!!}"
+            />
+            
           @endforeach 
          
-          </div>
+          </x-starterkid-frontend::card-grid>
           
  
       
