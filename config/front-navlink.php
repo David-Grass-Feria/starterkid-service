7614ca1d7aa5e5bb458b5d76ef3057323de6d1fe
David <kontakt@david-grass-feria.de>
2024-04-23 20:54:06 +0200

    1.0

--- config/front-navlink.php
+++ config/front-navlink.php
@@ -2,7 +2,7 @@
 
 return [
     [
-        'title' => env('SERVICE_TITLE','Service'),
+        'title' => env('SERVICE_MENU_NAME','Service'),
         'route' => 'front.service.index',
         'active' => ['front.service.index','front.service.show'],
         'order' => env('SERIVCE_ORDER_FOR_NAV',2),
--- config/starterkid-service.php
+++ config/starterkid-service.php
@@ -5,6 +5,7 @@ return
 [
     'service_slug'                   => env('SERVICE_SLUG','services'),
     'service_title'                  => env('SERVICE_TITLE','Service'),
+    'service_menu_name'             => env('SERVICE_MENU_NAME','Service'),
     'service_description'            => env('SERVICE_DESCRIPTION','Hier findest Du nützliche Informationen.'),
     
     
