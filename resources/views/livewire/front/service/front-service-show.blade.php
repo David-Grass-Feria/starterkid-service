b7788f15893da91fae42b799f6405f81bf730fe3
David <kontakt@david-grass-feria.de>
2024-04-28 15:24:10 +0200

    Update front-service-show.blade.php

--- resources/views/livewire/front/service/front-service-show.blade.php
+++ resources/views/livewire/front/service/front-service-show.blade.php
@@ -27,7 +27,7 @@
     @section('meta')
 <meta property="og:title" content="{{$service->name}}" />
 <meta property="og:description" content="{{ strip_tags($service->preview) ?? '' }}" />
-<meta property="og:image" content="{{$service->getFirstMediaUrl('ckimages','large') ?? ''}}" />
+<meta property="og:image" content="{{$service->getFirstMediaUrl('ck-images','large') ?? ''}}" />
 <meta property="og:url" content="{{url()->current()}}" />
 <meta property="og:type" content="website" />
 @endsection
